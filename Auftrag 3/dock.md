2f32012c3c734b8b771e1acc94947c1eff4896ba
mogls <154231651+mogls@users.noreply.github.com>
2025-06-10 15:50:49 +0200

    fixed path?

--- Auftrag 3/dock.md
+++ Auftrag 3/dock.md
@@ -6,3 +6,5 @@ ChatGPT benutzt um zu bestimmen wie ich routing fur die 10 projekte machen soll
 auch benutzt zum wissen wie ich header und footer xux xxlx sxxtxx axxxixen xxxx  
 
 Copilot fur header und footer styles  
+
+use `window.<ownFunc>` to make function accessible through a script tag  
--- Auftrag 3/hf.js
+++ Auftrag 3/hf.js
@@ -1,9 +1,19 @@
 function loadHTML(id, url) {
+    console.log("2nd");
     fetch(url)
         .then((response) => response.text())
         .then((data) => (document.getElementById(id).innerHTML = data))
         .catch((err) => console.error(err));
 }
 
-loadHTML("header-template", "./hf/header.html");
-loadHTML("footer-template", "./hf/footer.html");
+function loadTemplateWithPath(path) {
+    console.log("1st");
+    loadHTML("header-template", `${path}/hf/header.html`);
+    loadHTML("footer-template", `${path}/hf/footer.html`);
+}
+
+window.loadTemplateWithPath = loadTemplateWithPath;
+
+
+
+
